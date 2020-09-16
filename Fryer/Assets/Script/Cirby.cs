5202d1baba91641c4fcce0042f09f2242c71d3db
Realerz <pongsakon.jind@bumail.net>
2020-09-16 22:25:16 +0700

    First Cheack

---
+++ Fryer/Assets/Script/Cirby.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cirby : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    public Vector2 velocity;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update()
+    {
+        bool mouseDown = Input.GetMouseButtonDown(0);
+
+        if (mouseDown)
+        {
+            rigidbody2D.velocity = velocity;
+        }
+        
+    }
+}
